b4e215a75c301a9472eab6117a1be593c3b0b3f4
irchamzah <71950891+irchamzah@users.noreply.github.com>
2024-06-26 14:59:02 +0700

    Update page.tsx

--- src/app/detail_profile/page.tsx
+++ src/app/detail_profile/page.tsx
@@ -7,12 +7,12 @@ import Reviews from '@/components/detail_service/Reviews';
 export default function detail_service() {
   return (
     <Layout>
-      <Navbar mode={'block'} />
-      <GetToKnow header=' ' />
+      {/* <Navbar mode={'block'} />
+      <GetToKnow header=' ' /> */}
       <h1 className='mx-6 mb-5 max-w-7xl text-2xl font-bold text-gray-700 xl:mx-auto'>
         My Services
       </h1>
-      <ProductContainer />
+      {/* <ProductContainer /> */}
       <Reviews />
     </Layout>
   );
