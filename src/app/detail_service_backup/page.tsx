1f7c0cc278ebe27353224035134df96976a7ae26
irchamzah <71950891+irchamzah@users.noreply.github.com>
2024-06-26 14:55:37 +0700

    Update page.tsx

--- src/app/detail_service_backup/page.tsx
+++ src/app/detail_service_backup/page.tsx
@@ -1,34 +1,34 @@
-import Layout from '@/components/Layout';
-import Navbar from '@/components/Navbar';
-import BreadCrumbs from '@/components/all_services/BreadCrumbs';
-import GetToKnow from '@/components/detail_service/GetToKnow';
-import PhotoSlider from '@/components/detail_service/PhotoSlider';
-import ProfileCard from '@/components/detail_service/ProfileCard';
-import Reviews from '@/components/detail_service/Reviews';
-import ServiceDescriptionDetail from '@/components/detail_service/ServiceDesciptionDetail';
+// import Layout from '@/components/Layout';
+// import Navbar from '@/components/Navbar';
+// import BreadCrumbs from '@/components/all_services/BreadCrumbs';
+// import GetToKnow from '@/components/detail_service/GetToKnow';
+// import PhotoSlider from '@/components/detail_service/PhotoSlider';
+// import ProfileCard from '@/components/detail_service/ProfileCard';
+// import Reviews from '@/components/detail_service/Reviews';
+// import ServiceDescriptionDetail from '@/components/detail_service/ServiceDesciptionDetail';
 
-export default function detail_service() {
-  const images = [
-    '/images/random_image/img1.jpg',
-    '/images/random_image/img2.jpg',
-    '/images/random_image/img3.jpg',
-    '/images/random_image/img4.jpg',
-    '/images/random_image/img5.jpg',
-  ];
-  return (
-    <Layout>
-      <Navbar mode={'block'} />
-      <BreadCrumbs />
-      <div className='mx-auto max-w-7xl'>
-        <h1 className='mx-6 mb-4 mt-8 text-2xl font-semibold text-gray-700 xl:mx-0'>
-          I will create react website landing pages front end developer
-        </h1>
-      </div>
-      <ProfileCard />
-      <PhotoSlider images={images} />
-      <ServiceDescriptionDetail />
-      <GetToKnow header='Get to Know Malik' />
-      <Reviews />
-    </Layout>
-  );
-}
+// export default function detail_service() {
+//   const images = [
+//     '/images/random_image/img1.jpg',
+//     '/images/random_image/img2.jpg',
+//     '/images/random_image/img3.jpg',
+//     '/images/random_image/img4.jpg',
+//     '/images/random_image/img5.jpg',
+//   ];
+//   return (
+//     <Layout>
+//       <Navbar mode={'block'} />
+//       <BreadCrumbs />
+//       <div className='mx-auto max-w-7xl'>
+//         <h1 className='mx-6 mb-4 mt-8 text-2xl font-semibold text-gray-700 xl:mx-0'>
+//           I will create react website landing pages front end developer
+//         </h1>
+//       </div>
+//       <ProfileCard />
+//       <PhotoSlider images={images} />
+//       <ServiceDescriptionDetail />
+//       <GetToKnow header='Get to Know Malik' />
+//       <Reviews />
+//     </Layout>
+//   );
+// }
