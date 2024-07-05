f02913cf626f057afd7b35a6d0b6e20421ee80a7
irchamzah <71950891+irchamzah@users.noreply.github.com>
2024-07-05 07:42:34 +0700

    Revert "test Global state"
    This reverts commit 7352df9138dba2d3d046922f8bd26c7cbadd06a3.

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,18 +2,15 @@ import { ReactNode } from 'react';
 import Footer from './Footer';
 import MoreInfoSection from './categories_components/MoreInfoSection';
 import { fetchCategories } from '@/lib/actions/category.actions';
-import { GlobalStateProvider } from '@/contexts/GlobalStateContext';
 
 const Layout = async ({ children }: { children: ReactNode }) => {
   const categories = (await fetchCategories()) || [];
   return (
-    <GlobalStateProvider>
-      <div>
-        <div>{children}</div>
-        <MoreInfoSection categories={categories} />
-        <Footer />
-      </div>
-    </GlobalStateProvider>
+    <div>
+      <div>{children}</div>
+      <MoreInfoSection categories={categories} />
+      <Footer />
+    </div>
   );
 };
 
