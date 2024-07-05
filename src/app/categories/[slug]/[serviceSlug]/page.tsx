519898ae9bc8f6e8c8eae7eaab820dac1b711698
irchamzah <71950891+irchamzah@users.noreply.github.com>
2024-07-05 11:21:41 +0700

    trying to reduce request  HTTP time

--- src/app/categories/[slug]/[serviceSlug]/page.tsx
+++ src/app/categories/[slug]/[serviceSlug]/page.tsx
@@ -15,62 +15,65 @@ export default async function detail_service({
 }: {
   params: { serviceSlug: string };
 }) {
-  const categories = await fetchCategories();
-  if (!categories) {
-    return <div>categories tidak ditemukan...</div>;
-  }
+  try {
+    const [categories, service, ratingReviewSeller] = await Promise.all([
+      fetchCategories(),
+      getServiceBySlug(params.serviceSlug),
+      avgRatingCountReviewServiceByServiceSlug(params.serviceSlug),
+    ]);
 
-  const service = await getServiceBySlug(params.serviceSlug);
-  if (!service) {
+    if (!categories) {
+      return <div>categories tidak ditemukan...</div>;
+    }
+    if (!service) {
+      return (
+        <Layout>
+          <Navbar mode={'block'} categories={categories} />
+          <div className='mx-auto flex h-96 max-w-7xl items-center justify-center'>
+            <div className='mx-auto items-center text-center text-2xl font-semibold text-gray-700 xl:mx-0'>
+              Service tidak ditemukan.
+            </div>
+          </div>
+        </Layout>
+      );
+    }
+    if (!ratingReviewSeller) {
+      return <div>ratingReviewSeller tidak ditemukan...</div>;
+    }
+
     return (
       <Layout>
         <Navbar mode={'block'} categories={categories} />
-        <div className='mx-auto flex h-96 max-w-7xl items-center justify-center'>
-          <div className='mx-auto items-center text-center text-2xl font-semibold text-gray-700 xl:mx-0'>
-            Service tidak ditemukan.
-          </div>
+        <BreadCrumbs
+          categorySlug={service.category.slug}
+          categoryName={service.category.name}
+          currentServiceId={service.id}
+          currentServiceSlug={service.slug}
+        />
+        <div className='mx-auto max-w-7xl'>
+          <h1 className='mx-6 mb-4 mt-8 text-2xl font-semibold capitalize text-gray-700 xl:mx-0'>
+            {service.title}
+          </h1>
         </div>
+        <ProfileCard
+          profileData={service.author}
+          ratingAvg={ratingReviewSeller.averageRating}
+          reviewCount={ratingReviewSeller.countReviews}
+        />
+        <PhotoSlider images={service.servicePortfolio} />
+        <ServiceDescriptionDetail description={service.description} />
+        <GetToKnow
+          header={`Tentang ${service.author.name}`}
+          userId={service.authorId}
+        />
+        <Reviews
+          userId={''}
+          serviceId={service.id}
+          reviewsFor={'reviews for this service.'}
+        />
       </Layout>
     );
+  } catch (error) {
+    return <div>Terjadi kesalahan: </div>;
   }
-
-  const ratingReviewSeller = await avgRatingCountReviewServiceByServiceSlug(
-    params.serviceSlug
-  );
-  if (!ratingReviewSeller) {
-    return <div>ratingReviewSeller tidak ditemukan...</div>;
-  }
-
-  return (
-    <Layout>
-      <Navbar mode={'block'} categories={categories} />
-      <BreadCrumbs
-        categorySlug={service.category.slug}
-        categoryName={service.category.name}
-        currentServiceId={service.id}
-        currentServiceSlug={service.slug}
-      />
-      <div className='mx-auto max-w-7xl'>
-        <h1 className='mx-6 mb-4 mt-8 text-2xl font-semibold capitalize text-gray-700 xl:mx-0'>
-          {service.title}
-        </h1>
-      </div>
-      <ProfileCard
-        profileData={service.author}
-        ratingAvg={ratingReviewSeller.averageRating}
-        reviewCount={ratingReviewSeller.countReviews}
-      />
-      <PhotoSlider images={service.servicePortfolio} />
-      <ServiceDescriptionDetail description={service.description} />
-      <GetToKnow
-        header={`Tentang ${service.author.name}`}
-        userId={service.authorId}
-      />
-      <Reviews
-        userId={''}
-        serviceId={service.id}
-        reviewsFor={'reviews for this service.'}
-      />
-    </Layout>
-  );
 }
